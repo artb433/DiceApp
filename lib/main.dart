129046c43033283ec971aaae1c5e75a31f7bdd4b
Art b <94374097+artb433@users.noreply.github.com>
2021-12-23 13:42:28 +0000

    my

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dicee/two_player.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -6,7 +8,16 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
-          title: Text('Dicee'),
+          actions: [
+            InkWell(
+                onTap: () {
+                  BuildContext context;
+                  Navigator.of(context).push(
+                      MaterialPageRoute(builder: (buildContext) => Computer()));
+                },
+                child: Icon(Icons.computer))
+          ],
+          title: Text('Art B Technologies'),
           backgroundColor: Colors.teal,
         ),
         body: DicePage(),
@@ -15,7 +26,14 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -25,17 +43,23 @@ class DicePage extends StatelessWidget {
             // ignore: deprecated_member_use
             child: FlatButton(
                 onPressed: () {
-                  print('You pressed the first dice');
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                    print('dicenmber = $leftDiceNumber');
+                  });
                 },
-                child: Image.asset('images/dice1.png')),
+                child: Image.asset('images/dice$leftDiceNumber.png')),
           ),
           Expanded(
               // ignore: deprecated_member_use
               child: FlatButton(
                   onPressed: () {
-                    print('you pressed the second dice');
+                    setState(() {
+                      rightDiceNumber = Random().nextInt(6) + 1;
+                      print('diceNumber = $rightDiceNumber.png');
+                    });
                   },
-                  child: Image.asset('images/dice1.png'))),
+                  child: Image.asset('images/dice$rightDiceNumber.png'))),
         ],
       ),
     );
